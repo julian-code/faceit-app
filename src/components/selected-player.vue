13c751d6e5e76268367d92cff09d96627f6578cf
julian-code <54644662+julian-code@users.noreply.github.com>
2020-03-17 00:09:38 +0100

    getting all data when adding player

--- src/components/selected-player.vue
+++ src/components/selected-player.vue
@@ -1,92 +1,146 @@
 <template>
-  <v-card class="pl-2">
-    <v-row>
-      <v-col cols="2" style="text-align: center">
-        <v-avatar color="grey" size="164">
-          <img :src="player.avatar" />
-        </v-avatar>
-        <v-list-item dark>
-          <v-list-item-content>
-            <v-list-item-title class="title">{{player.nickname}}</v-list-item-title>
-            <v-list-item-subtitle>Level: {{player.csgo_skill_level}}</v-list-item-subtitle>
-            <v-list-item-subtitle>ELO: {{player.games.csgo.faceit_elo}}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-col>
-      <v-col cols="10">
-        <v-card class="pr-2 pl-2 mt-0" elevation="0">
-          <v-row>
-            <v-col>
-              <v-card>
-                <v-card-text class="text-center">
-                  <p class="dispay-1 text--primary">Maps played:</p>
-                  <p class="text--primary">{{player.stats.m1}}</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text class="text-center">
-                  <p class="dispay-1 text--primary">Win %:</p>
-                  <p class="text--primary">{{player.stats.k6}}</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text class="text-center">
-                  <p class="dispay-1 text--primary">Average K/D:</p>
-                  <p class="text--primary">{{player.stats.k5}}</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text class="text-center">
-                  <p class="dispay-1 text--primary">Average HS %:</p>
-                  <p class="text--primary">{{player.stats.k8}}</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text class="text-center">
-                  <p class="dispay-1 text--primary">5 recent matches:</p>
-                  <p
-                    class="text--primary"
-                  >{{recentResults.wins}} Win(s), {{recentResults.losses}} Loss(es)</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text class="text-center">
-                  <p class="dispay-1 text--primary">Longest win streak:</p>
-                  <p class="text--primary">{{player.stats.s2}}</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-data-table :items="matches"></v-data-table>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-card>
+  <v-skeleton-loader
+    :loading="loading"
+    :transition="transition"
+    type="card"
+  >
+    <v-card class="pl-2">
+      <v-row>
+        <v-col cols="2" style="text-align: center">
+          <v-avatar color="grey" size="164">
+            <img :src="player.avatar" />
+          </v-avatar>
+          <v-list-item dark>
+            <v-list-item-content>
+              <v-list-item-title class="title">{{player.nickname}}</v-list-item-title>
+              <v-list-item-subtitle>Level: {{player.csgo_skill_level}}</v-list-item-subtitle>
+              <v-list-item-subtitle>ELO: {{player.games.csgo.faceit_elo}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col cols="10">
+          <v-card class="pr-2 pl-2 mt-0" elevation="0">
+            <v-row>
+              <v-col>
+                <v-card>
+                  <v-card-text class="text-center">
+                    <p class="dispay-1 text--primary">Maps played:</p>
+                    <p class="text--primary">{{player.stats.m1}}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card>
+                  <v-card-text class="text-center">
+                    <p class="dispay-1 text--primary">Win %:</p>
+                    <p class="text--primary">{{player.stats.k6}}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card>
+                  <v-card-text class="text-center">
+                    <p class="dispay-1 text--primary">Average K/D:</p>
+                    <p class="text--primary">{{player.stats.k5}}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card>
+                  <v-card-text class="text-center">
+                    <p class="dispay-1 text--primary">Average HS %:</p>
+                    <p class="text--primary">{{player.stats.k8}}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card>
+                  <v-card-text class="text-center">
+                    <p class="dispay-1 text--primary">5 recent matches:</p>
+                    <p
+                      class="text--primary"
+                    >{{recentResults.wins}} Win(s), {{recentResults.losses}} Loss(es)</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card>
+                  <v-card-text class="text-center">
+                    <p class="dispay-1 text--primary">Longest win streak:</p>
+                    <p class="text--primary">{{player.stats.s2}}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-data-table
+                  :loading="loading"
+                  :headers="headers"
+                  :items="player.matches"
+                  @click:row="openMatch"
+                >
+                  <template v-slot:item.i10="{ item }">
+                    <v-chip v-if="item.i10 > 0" :color="determineWin(item.i10)" dark>WIN</v-chip>
+                    <v-chip v-else :color="determineWin(item.i10)" dark>LOSS</v-chip>
+                  </template>
+                </v-data-table>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-skeleton-loader>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data() {
     return {
       tab: null,
-      entries: [],
+      headers: [
+        {
+          text: 'Map',
+          value: 'i1',
+        },
+        {
+          text: 'Win',
+          value: 'i10',
+        },
+        {
+          text: 'Elo',
+          value: 'elo',
+        },
+        {
+          text: 'Kills',
+          value: 'i6',
+        },
+        {
+          text: 'Assists',
+          value: 'i7',
+        },
+        {
+          text: 'Deaths',
+          value: 'i8',
+        },
+        {
+          text: 'K/R',
+          value: 'c3',
+        },
+        {
+          text: 'K/D',
+          value: 'c2',
+        },
+      ],
     };
   },
   computed: {
+    ...mapState({
+      loading: (state) => state.isLoading,
+    }),
     recentResults() {
       const total = this.player.stats.s0.reduce(
         (match1, match2) => Number(match1) + Number(match2),
@@ -94,20 +148,6 @@ export default {
       );
       return { wins: total, losses: 5 - total };
     },
-    matches() {
-      let matchConcat = [];
-      this.entries.forEach((entry) => {
-        if (matchConcat.length > 0) {
-          matchConcat = matchConcat.concat(entry);
-        } else {
-          matchConcat = entry;
-        }
-      });
-      return matchConcat;
-    },
-  },
-  async created() {
-    await this.getMatches();
   },
   props: {
     player: {
@@ -116,23 +156,18 @@ export default {
     },
   },
   methods: {
-    async getMatches() {
-      const pages = this.player.stats.m1 / 2000;
-      console.log(pages);
-      const results = [];
-      for (let index = 0; index < pages; index += 1) {
-        results.push(this.fetchMatches(index));
-      }
-      this.entries = await Promise.all(results);
+    openMatch(match) {
+      window.open(
+        `https://www.faceit.com/en/csgo/room/${match.matchId}`,
+        '_blank',
+      );
     },
-    async fetchMatches(pageNumber) {
-      const response = await fetch(`https://api.faceit.com/stats/v1/stats/time/users/${this.player.guid}/games/csgo?page=${pageNumber}&size=2000`);
-      const data = response.json();
-      return data;
+    determineWin(winState) {
+      if (winState > 0) {
+        return 'green';
+      }
+      return 'red';
     },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,14 @@ import { ACTIONS, MUTATIONS } from './constants';
 
 Vue.use(Vuex);
 
+async function fetchMatches(pageNumber, player) {
+  const response = await fetch(
+    `https://api.faceit.com/stats/v1/stats/time/users/${player.guid}/games/csgo?page=${pageNumber}&size=2000`,
+  );
+  const data = response.json();
+  return data;
+}
+
 async function getPlayerProfile(playerName) {
   const response = await fetch(`https://api.faceit.com/core/v1/nicknames/${playerName}`);
   const data = await response.json();
@@ -11,6 +19,21 @@ async function getPlayerProfile(playerName) {
   const response2 = await fetch(`https://api.faceit.com/stats/api/v1/stats/users/${player.guid}/games/csgo`);
   const stats = await response2.json();
   player.stats = stats.lifetime;
+  const pages = player.stats.m1 / 2000;
+  const results = [];
+  for (let index = 0; index < pages; index += 1) {
+    results.push(fetchMatches(index, player));
+  }
+  const matches = await Promise.all(results);
+  let matchConcat = [];
+  matches.forEach((entry) => {
+    if (matchConcat.length > 0) {
+      matchConcat = matchConcat.concat(entry);
+    } else {
+      matchConcat = entry;
+    }
+  });
+  player.matches = matchConcat;
   return player;
 }
 
@@ -18,8 +41,12 @@ export default new Vuex.Store({
   state: {
     drawer: false,
     selectedPlayers: [],
+    isLoading: false,
   },
   mutations: {
+    [MUTATIONS.SET_IS_LOADING](state, payload) {
+      state.isLoading = payload;
+    },
     [MUTATIONS.SET_DRAWER](state, payload) {
       state.drawer = payload;
     },
@@ -43,7 +70,9 @@ export default new Vuex.Store({
         const element = payload[index];
         results.push(getPlayerProfile(element));
       }
+      ctx.commit(MUTATIONS.SET_IS_LOADING, true);
       ctx.commit(MUTATIONS.SET_SELECTED_PLAYERS, await Promise.all(results));
+      ctx.commit(MUTATIONS.SET_IS_LOADING, false);
     },
     [ACTIONS.REMOVE_SELECTED_PLAYER](ctx, payload) {
       ctx.commit(MUTATIONS.REMOVE_SELECTED_PLAYER, payload);
